ae981b1f1275486045634ed3ba0ad055ecbd314d
xyzabc0000001 <93307200+xyzabc0000001@users.noreply.github.com>
2021-11-06 20:42:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+document.querySelector(".emoji").style.display = "none";
+$(window).on('load', function () {
+  document.querySelector(".img").style.display = "none";
+  document.querySelector(".emoji").style.display = "none";
+});
+
+window.onresize = function(){
+  if(window.width() > 1024){
+    var x = document.getElementsByTagName("BODY")[0];
+    x.style.backgroundColor = "#221f1f";
+    
+    window.location.replace("https://www.google.com")
+  }
+}
+
+
+setTimeout(function () {
+  document.querySelector(".img").style.display = "none";
+  document.querySelector(".emoji").style.display = "flex";
+}, 7000);
+
+//emoji
+const emoji = document.querySelector(".emoji");
+var display = document.getElementsByClassName("emoji");
+var emojiList = ["🥳", "🤩", "👾", "😵", "👨‍💻", "🎮", "🖕"];
+var i = 0;
+var timer;
+
+window.onload = function randomEmoji() {
+  clearInterval(timer);
+
+  // Call show Emoji to let it shows instanly.
+  showEmoji();
+
+  // Put showEmoji function to let it repeats
+  timer = setInterval(function () {
+    showEmoji();
+  }, 25);
+}
+
+function showEmoji() {
+  i = Math.floor(Math.random() * emojiList.length);
+  emoji.innerHTML = emojiList[i];
+}
+
+setTimeout(function () {
+  // clear interval timer to let it stops
+  clearInterval(timer);
+  emoji.innerHTML = emojiList[6]
+}, 10000);
